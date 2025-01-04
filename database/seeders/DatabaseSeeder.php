931a3bd0b01b6a85d4ba453710a42f372110398c
pejoal <pejoal.official@gmail.com>
2025-01-04 18:30:32 +0100

    Add username field to UserFactory and update DatabaseSeeder with sample users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,18 +6,21 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->withPersonalTeam()->create();
+class DatabaseSeeder extends Seeder {
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void {
+    // User::factory(10)->withPersonalTeam()->create();
 
-        User::factory()->withPersonalTeam()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    User::factory()->withPersonalTeam()->create([
+      'name' => 'Pejoal Hanna',
+      'username' => 'pejoal',
+      'email' => 'pejoal.official@gmail.com',
+    ]);
+    User::factory()->create([
+      'name' => 'Jane',
+      'username' => 'jane',
+    ]);
+  }
 }
