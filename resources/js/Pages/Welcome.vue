52ac19e7ef8f9e96ed7ee931914524afd9bcfeb0
pejoal <pejoal.official@gmail.com>
2025-01-07 02:07:58 +0100

    Refactor Welcome Page 1

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -8,14 +8,6 @@ defineProps({
   canRegister: {
     type: Boolean,
   },
-  laravelVersion: {
-    type: String,
-    required: true,
-  },
-  phpVersion: {
-    type: String,
-    required: true,
-  },
 });
 </script>
 
@@ -31,36 +23,36 @@ defineProps({
       class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white"
     >
       <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-        <header
-          class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3"
-        >
-          <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
+        <h1 class="text-2xl font-medium text-gray-900 dark:text-white">
+          Welcome to your Company Management System!
+        </h1>
+
+        <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
+          <Link
+            v-if="$page.props.auth.user"
+            :href="route('dashboard')"
+            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+          >
+            Dashboard
+          </Link>
+
+          <template v-else>
             <Link
-              v-if="$page.props.auth.user"
-              :href="route('dashboard')"
+              :href="route('login')"
               class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
             >
-              Dashboard
+              Log in
             </Link>
 
-            <template v-else>
-              <Link
-                :href="route('login')"
-                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-              >
-                Log in
-              </Link>
-
-              <Link
-                v-if="canRegister"
-                :href="route('register')"
-                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-              >
-                Register
-              </Link>
-            </template>
-          </nav>
-        </header>
+            <Link
+              v-if="canRegister"
+              :href="route('register')"
+              class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+            >
+              Register
+            </Link>
+          </template>
+        </nav>
       </div>
     </div>
   </div>
