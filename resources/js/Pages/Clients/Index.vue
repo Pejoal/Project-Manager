990bca0fc8c5b164129da9ee235017f36bc6a0e6
pejoal <pejoal.official@gmail.com>
2025-01-07 22:37:13 +0100

    Client Management 2

--- resources/js/Pages/Clients/Index.vue
+++ resources/js/Pages/Clients/Index.vue
@@ -1,18 +1,33 @@
 <template>
-  <div>
-    <h1>Clients</h1>
-    <ul>
-      <li v-for="client in clients" :key="client.id">
-        <router-link :to="{ name: 'clients.show', params: { client: client.id } }">
-          {{ client.name }}
-        </router-link>
-      </li>
-    </ul>
-    <router-link :to="{ name: 'clients.create' }">Add New Client</router-link>
-  </div>
+  <AppLayout>
+    <template #header>
+      <h1 class="text-2xl font-semibold text-gray-900 dark:text-gray-100">Clients</h1>
+    </template>
+    <div class="p-6 bg-white dark:bg-gray-800 rounded-lg shadow-md">
+      <ul class="mb-4">
+        <li v-for="client in clients" :key="client.id" class="mb-2">
+          <Link
+            :href="route('clients.show', client.id)"
+            class="text-blue-500 dark:text-blue-400 hover:underline"
+          >
+            {{ client.name }}
+          </Link>
+        </li>
+      </ul>
+      <Link
+        :href="route('clients.create')"
+        class="text-blue-500 dark:text-blue-400 hover:underline"
+      >
+        Add New Client
+      </Link>
+    </div>
+  </AppLayout>
 </template>
 
 <script setup>
+import { Link } from '@inertiajs/vue3';
+import AppLayout from '@/Layouts/AppLayout.vue';
+
 defineProps({
   clients: Array,
 });
--- resources/js/Pages/Clients/Show.vue
+++ resources/js/Pages/Clients/Show.vue
@@ -1,16 +1,31 @@
 <template>
-  <div>
-    <h1>{{ client.name }}</h1>
-    <p>Email: {{ client.email }}</p>
-    <p>Phone: {{ client.phone }}</p>
-    <router-link :to="{ name: 'clients.edit', params: { client: client.id } }">Edit</router-link>
-    <button @click="destroy">Delete</button>
-  </div>
+  <AppLayout>
+    <template #header>
+      <h1 class="text-2xl font-semibold text-gray-900 dark:text-gray-100">{{ client.name }}</h1>
+    </template>
+    <div class="p-6 bg-white dark:bg-gray-800 rounded-lg shadow-md">
+      <p class="mb-2 text-gray-700 dark:text-gray-300">Email: {{ client.email }}</p>
+      <p class="mb-4 text-gray-700 dark:text-gray-300">Phone: {{ client.phone }}</p>
+      <Link
+        :href="route('clients.edit', client.id)"
+        class="text-blue-500 dark:text-blue-400 hover:underline"
+      >
+        Edit
+      </Link>
+      <button
+        @click="destroy"
+        class="ml-4 text-red-500 dark:text-red-400 hover:underline"
+      >
+        Delete
+      </button>
+    </div>
+  </AppLayout>
 </template>
 
 <script setup>
 import { defineProps } from 'vue';
-import { useForm } from '@inertiajs/vue3';
+import { useForm, Link } from '@inertiajs/vue3';
+import AppLayout from '@/Layouts/AppLayout.vue';
 
 const props = defineProps({
   client: Object,
