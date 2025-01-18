75c977399010d5730a731c9b704a36c7c64ed3d3
pejoal <pejoal.official@gmail.com>
2025-01-18 22:34:14 +0100

    Fix status name check in CreateTaskStatusModal for accurate display in title and button

--- resources/js/Pages/TaskStatuses/CreateTaskStatusModal.vue
+++ resources/js/Pages/TaskStatuses/CreateTaskStatusModal.vue
@@ -51,12 +51,15 @@ const submit = () => {
     });
   }
 };
+
+console.log(status.value?.name);
+
 </script>
 
 <template>
   <DialogModal :show="props.show" @close="emit('close')">
     <template #title
-      >{{ status?.value ? 'Edit' : 'Create' }} Task Status</template
+      >{{ status?.name ? 'Edit' : 'Create' }} Task Status</template
     >
     <template #content>
       <form id="form" @submit.prevent="submit" class="space-y-4">
@@ -97,7 +100,7 @@ const submit = () => {
         type="submit"
         class="ms-3 px-4 py-2 bg-blue-500 dark:bg-blue-600 text-white rounded-md hover:bg-blue-600 dark:hover:bg-blue-700"
       >
-        {{ status?.value ? 'Edit' : 'Create' }}
+        {{ status?.name ? 'Edit' : 'Create' }}
       </button>
     </template>
   </DialogModal>
