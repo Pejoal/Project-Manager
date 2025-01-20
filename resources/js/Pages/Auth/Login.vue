20183e645d0cbac31d098e526b8b39e221c65d41
pejoal <pejoal.official@gmail.com>
2025-01-20 19:05:20 +0100

    Add registration link to Login.vue

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -92,6 +92,13 @@ const submit = () => {
           Forgot your password?
         </Link>
 
+        <Link
+          :href="route('register')"
+          class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 ms-4"
+        >
+          Register
+        </Link>
+
         <PrimaryButton
           class="ms-4"
           :class="{ 'opacity-25': form.processing }"
