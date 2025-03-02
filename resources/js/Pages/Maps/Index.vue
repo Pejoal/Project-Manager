3d7bb69a3371f46fe38f174b5728556286d6cc45
pejoal <pejoal.official@gmail.com>
2025-03-02 17:47:11 +0100

    Implement feature to reset map preferences to default settings

--- resources/js/Pages/Maps/Index.vue
+++ resources/js/Pages/Maps/Index.vue
@@ -32,6 +32,7 @@ const selectedFeature = ref(null);
 const newPropertyKey = ref('');
 const newPropertyValue = ref('');
 const isFeaturesListOpen = ref(false);
+const isControlsOpen = ref(false);
 
 const filteredFeatures = computed(() => {
   return features.value.filter((feature) => {
@@ -49,6 +50,7 @@ const savePreferences = () => {
     addingLineString: addingLineString.value,
     addingPolygon: addingPolygon.value,
     isFeaturesListOpen: isFeaturesListOpen.value,
+    isControlsOpen: isControlsOpen.value,
     features: features.value,
   };
   localStorage.setItem('preferences', JSON.stringify(preferences));
@@ -66,6 +68,7 @@ const loadPreferences = () => {
     addingLineString.value = preferences.addingLineString;
     addingPolygon.value = preferences.addingPolygon;
     isFeaturesListOpen.value = preferences.isFeaturesListOpen;
+    isControlsOpen.value = preferences.isControlsOpen;
     features.value = preferences.features || [];
 
     // Re-add sources and layers for points, lines, and polygons
@@ -490,166 +493,181 @@ const addPropertyToFeature = () => {
 const toggleFeaturesList = () => {
   isFeaturesListOpen.value = !isFeaturesListOpen.value;
 };
+const toggleControls = () => {
+  isControlsOpen.value = !isControlsOpen.value;
+};
 </script>
 
 <template>
   <Head title="Maplibre Example" />
   <AppLayout>
-    <main class="mx-4">
-      <div class="mb-4">
-        <label for="mapStyle" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
-          >Select Map Style:</label
-        >
-        <select
-          id="mapStyle"
-          v-model="selectedStyle"
-          @change="changeStyle(selectedStyle)"
-          class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-        >
-          <option v-for="style in styles" :key="style.url" :value="style.url">{{ style.name }}</option>
-        </select>
-      </div>
-      <div class="mb-4">
-        <button @click="resetView" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
-          Reset View
-        </button>
-      </div>
-      <div class="mb-4">
+    <main>
+      <div class="p-4">
         <button
-          @click="toggleAddingPoints"
-          :class="{ 'bg-green-500': addingPoints, 'bg-gray-500': !addingPoints }"
+          @click="toggleControls"
+          :class="{ 'bg-green-500': isControlsOpen, 'bg-gray-500': !isControlsOpen }"
           class="px-4 py-2 text-white rounded-md hover:bg-green-600"
         >
-          {{ addingPoints ? 'Disable' : 'Enable' }} Adding Points
-        </button>
-        <button
-          @click="toggleAddingLineString"
-          :class="{ 'bg-green-500': addingLineString, 'bg-gray-500': !addingLineString }"
-          class="px-4 py-2 text-white rounded-md hover:bg-green-600 ml-2"
-        >
-          {{ addingLineString ? 'Disable' : 'Enable' }} Adding LineString
-        </button>
-        <button
-          @click="toggleAddingPolygon"
-          :class="{ 'bg-green-500': addingPolygon, 'bg-gray-500': !addingPolygon }"
-          class="px-4 py-2 text-white rounded-md hover:bg-green-600 ml-2"
-        >
-          {{ addingPolygon ? 'Disable' : 'Enable' }} Adding Polygon
-        </button>
-        <button @click="deleteFeatures" class="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 ml-2">
-          Delete All Features
+          {{ isControlsOpen ? 'Hide' : 'Show' }} Controls
         </button>
       </div>
-      <div class="mb-4">
-        <button
-          @click="toggleFeaturesList"
-          :class="{ 'bg-green-500': isFeaturesListOpen, 'bg-gray-500': !isFeaturesListOpen }"
-          class="px-4 py-2 text-white rounded-md hover:bg-green-600"
-        >
-          {{ isFeaturesListOpen ? 'Hide' : 'Show' }} Features List
-        </button>
-      </div>
-      <div v-if="isFeaturesListOpen" class="mb-4">
-        <input
-          type="text"
-          v-model="filterText"
-          placeholder="Filter features"
-          class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-        />
-        <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700 mt-4">
-          <thead class="bg-gray-50 dark:bg-gray-800">
-            <tr>
-              <th
-                scope="col"
-                class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider"
-              >
-                Name
-              </th>
-              <th
-                scope="col"
-                class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider"
-              >
-                Type
-              </th>
-              <th
-                scope="col"
-                class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider"
-              >
-                Actions
-              </th>
-            </tr>
-          </thead>
-          <tbody class="bg-white dark:bg-gray-900 divide-y divide-gray-200 dark:divide-gray-700">
-            <tr v-for="feature in filteredFeatures" :key="feature.properties.name">
-              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">
-                {{ feature.properties.name }}
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">
-                {{ feature.geometry.type }}
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                <button
-                  @click="selectFeature(feature)"
-                  class="text-indigo-600 dark:text-indigo-400 hover:text-indigo-900 dark:hover:text-indigo-600"
-                >
-                  Edit
-                </button>
-                <button
-                  @click="deleteFeature(feature)"
-                  class="text-red-600 dark:text-red-400 hover:text-red-900 dark:hover:text-red-600 ml-2"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div v-if="selectedFeature" class="mb-4">
-        <h2 class="text-xl font-semibold text-gray-900 dark:text-gray-100">
-          Edit Feature: {{ selectedFeature.properties.name }}
-        </h2>
-        <div class="mt-2">
-          <h3 class="text-lg font-medium text-gray-700 dark:text-gray-300">Properties:</h3>
-          <ul class="list-disc pl-5">
-            <li
-              v-for="(value, key) in selectedFeature.properties"
-              :key="key"
-              class="text-sm text-gray-700 dark:text-gray-300"
-            >
-              {{ key }}: {{ value }}
-            </li>
-          </ul>
-        </div>
-        <div class="mt-2">
-          <label for="newPropertyKey" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
-            >Property Key</label
+      <section class="mx-4" v-if="isControlsOpen">
+        <div class="mb-4">
+          <label for="mapStyle" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
+            >Select Map Style:</label
           >
-          <input
-            type="text"
-            id="newPropertyKey"
-            v-model="newPropertyKey"
+          <select
+            id="mapStyle"
+            v-model="selectedStyle"
+            @change="changeStyle(selectedStyle)"
             class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-          />
-        </div>
-        <div class="mt-2">
-          <label for="newPropertyValue" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
-            >Property Value</label
           >
-          <input
-            type="text"
-            id="newPropertyValue"
-            v-model="newPropertyValue"
-            class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-          />
+            <option v-for="style in styles" :key="style.url" :value="style.url">{{ style.name }}</option>
+          </select>
         </div>
-        <div class="mt-4">
-          <button @click="addPropertyToFeature" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
-            Add Property
+        <div class="mb-4">
+          <button @click="resetView" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+            Reset View
           </button>
         </div>
-      </div>
-      <div ref="mapContainer" class="map-container"></div>
+        <div class="mb-4">
+          <button
+            @click="toggleAddingPoints"
+            :class="{ 'bg-green-500': addingPoints, 'bg-gray-500': !addingPoints }"
+            class="px-4 py-2 text-white rounded-md hover:bg-green-600"
+          >
+            {{ addingPoints ? 'Disable' : 'Enable' }} Adding Points
+          </button>
+          <button
+            @click="toggleAddingLineString"
+            :class="{ 'bg-green-500': addingLineString, 'bg-gray-500': !addingLineString }"
+            class="px-4 py-2 text-white rounded-md hover:bg-green-600 ml-2"
+          >
+            {{ addingLineString ? 'Disable' : 'Enable' }} Adding LineString
+          </button>
+          <button
+            @click="toggleAddingPolygon"
+            :class="{ 'bg-green-500': addingPolygon, 'bg-gray-500': !addingPolygon }"
+            class="px-4 py-2 text-white rounded-md hover:bg-green-600 ml-2"
+          >
+            {{ addingPolygon ? 'Disable' : 'Enable' }} Adding Polygon
+          </button>
+          <button @click="deleteFeatures" class="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 ml-2">
+            Delete All Features
+          </button>
+        </div>
+        <div class="mb-4">
+          <button
+            @click="toggleFeaturesList"
+            :class="{ 'bg-green-500': isFeaturesListOpen, 'bg-gray-500': !isFeaturesListOpen }"
+            class="px-4 py-2 text-white rounded-md hover:bg-green-600"
+          >
+            {{ isFeaturesListOpen ? 'Hide' : 'Show' }} Features List
+          </button>
+        </div>
+        <div v-if="isFeaturesListOpen" class="mb-4">
+          <input
+            type="text"
+            v-model="filterText"
+            placeholder="Filter features"
+            class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+          />
+          <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700 mt-4">
+            <thead class="bg-gray-50 dark:bg-gray-800">
+              <tr>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider"
+                >
+                  Name
+                </th>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider"
+                >
+                  Type
+                </th>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider"
+                >
+                  Actions
+                </th>
+              </tr>
+            </thead>
+            <tbody class="bg-white dark:bg-gray-900 divide-y divide-gray-200 dark:divide-gray-700">
+              <tr v-for="feature in filteredFeatures" :key="feature.properties.name">
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">
+                  {{ feature.properties.name }}
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">
+                  {{ feature.geometry.type }}
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                  <button
+                    @click="selectFeature(feature)"
+                    class="text-indigo-600 dark:text-indigo-400 hover:text-indigo-900 dark:hover:text-indigo-600"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    @click="deleteFeature(feature)"
+                    class="text-red-600 dark:text-red-400 hover:text-red-900 dark:hover:text-red-600 ml-2"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div v-if="selectedFeature" class="mb-4">
+          <h2 class="text-xl font-semibold text-gray-900 dark:text-gray-100">
+            Edit Feature: {{ selectedFeature.properties.name }}
+          </h2>
+          <div class="mt-2">
+            <h3 class="text-lg font-medium text-gray-700 dark:text-gray-300">Properties:</h3>
+            <ul class="list-disc pl-5">
+              <li
+                v-for="(value, key) in selectedFeature.properties"
+                :key="key"
+                class="text-sm text-gray-700 dark:text-gray-300"
+              >
+                {{ key }}: {{ value }}
+              </li>
+            </ul>
+          </div>
+          <div class="mt-2">
+            <label for="newPropertyKey" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              >Property Key</label
+            >
+            <input
+              type="text"
+              id="newPropertyKey"
+              v-model="newPropertyKey"
+              class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            />
+          </div>
+          <div class="mt-2">
+            <label for="newPropertyValue" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              >Property Value</label
+            >
+            <input
+              type="text"
+              id="newPropertyValue"
+              v-model="newPropertyValue"
+              class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            />
+          </div>
+          <div class="mt-4">
+            <button @click="addPropertyToFeature" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+              Add Property
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <section ref="mapContainer" class="map-container"></section>
     </main>
   </AppLayout>
 </template>
