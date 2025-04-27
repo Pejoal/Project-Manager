bb221eca6cf32963af4ae34d75da282fd559ad82
pejoal <pejoal.official@gmail.com>
2025-04-27 20:23:28 +0200

    Adds chart color customization options
    Allows users to customize the colors of charts displayed on the dashboard.
    
    This enhancement improves the user experience by enabling personalized visual representations of data.

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -41,107 +41,62 @@ onMounted(() => {
   //   console.log('Message received:', data.message);
   // });
 });
-
 </script>
 
 <template>
   <AppLayout title="Dashboard">
     <template #header>
-      <h2
-        class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"
-      >
+      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
         {{ translations.welcome }} {{ $page.props.auth.user.name }}
       </h2>
     </template>
 
     <div class="max-w-7xl mx-auto sm:px-1 lg:px-8">
-      <div
-        class="p-2 my-1 bg-white dark:text-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg"
-      >
+      <div class="p-2 my-1 bg-white dark:text-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
         <div class="mb-2 pb-2 border-b">
-          <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-4">
-            Update Chart Colors
-          </h3>
+          <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-4">Update Chart Colors</h3>
           <form @submit.prevent="updateSettings">
             <div class="mb-4">
-              <label
-                for="clients_color"
-                class="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              <label for="clients_color" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
                 >Clients Color</label
               >
-              <input
-                v-model="form.clients_color"
-                type="color"
-                id="clients_color"
-                class="mt-1 block w-full"
-              />
+              <input v-model="form.clients_color" type="color" id="clients_color" class="mt-1 block w-full" />
             </div>
             <div class="mb-4">
-              <label
-                for="projects_color"
-                class="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              <label for="projects_color" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
                 >Projects Color</label
               >
-              <input
-                v-model="form.projects_color"
-                type="color"
-                id="projects_color"
-                class="mt-1 block w-full"
-              />
+              <input v-model="form.projects_color" type="color" id="projects_color" class="mt-1 block w-full" />
             </div>
             <div class="mb-4">
-              <label
-                for="tasks_color"
-                class="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              <label for="tasks_color" class="block text-sm font-medium text-gray-700 dark:text-gray-300"
                 >Tasks Color</label
               >
-              <input
-                v-model="form.tasks_color"
-                type="color"
-                id="tasks_color"
-                class="mt-1 block w-full"
-              />
+              <input v-model="form.tasks_color" type="color" id="tasks_color" class="mt-1 block w-full" />
             </div>
-            <button
-              type="submit"
-              :disabled="form.processing"
-              class="px-4 py-2 bg-blue-500 text-white rounded"
-            >
+            <button type="submit" :disabled="form.processing" class="px-4 py-2 bg-blue-500 text-white rounded">
               Save
             </button>
           </form>
         </div>
-        <div class="mb-8">
-          <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-4">
-            Overview
-          </h3>
+        <div class="mb-4 pb-2 border-b">
+          <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-4">Overview</h3>
           <LineChart :chart-data="lineChartData" :options="chartOptions" />
         </div>
-        <div class="mb-8">
-          <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-4">
-            Tasks Statuses
-          </h3>
+        <div class="mb-4 pb-2 border-b">
+          <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-4">Tasks Statuses</h3>
           <BarChart :chart-data="tasksStatusesData" :options="chartOptions" />
         </div>
-        <div class="mb-8">
-          <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-4">
-            Tasks Priorities
-          </h3>
-          <LineChart
-            :chart-data="tasksPrioritiesData"
-            :options="chartOptions"
-          />
+        <div class="mb-4 pb-2 border-b">
+          <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-4">Tasks Priorities</h3>
+          <LineChart :chart-data="tasksPrioritiesData" :options="chartOptions" />
         </div>
-        <div class="mb-8">
-          <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-4">
-            Clients, Projects, and Tasks
-          </h3>
+        <div class="mb-4 pb-2 border-b">
+          <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-4">Clients, Projects, and Tasks</h3>
           <DoughnutChart :chart-data="doughnutData" :options="chartOptions" />
         </div>
-        <div class="mb-8">
-          <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-4">
-            Clients, Projects, and Tasks
-          </h3>
+        <div class="mb-4 pb-2 border-b">
+          <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-4">Clients, Projects, and Tasks</h3>
           <PieChart :chart-data="doughnutData" :options="chartOptions" />
         </div>
       </div>
@@ -149,5 +104,4 @@ onMounted(() => {
   </AppLayout>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
