860ef91e88b406b63828ceac8ac730479dc895a2
pejoal <pejoal.official@gmail.com>
2025-04-27 20:57:08 +0200

    center dark mode button

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -69,7 +69,7 @@ const toggleDarkMode = () => {
 
                 <NavLink :href="route('tasks.all')" :active="route().current('tasks.*')"> Tasks </NavLink>
 
-                <div class="ms-3 relative">
+                <div class="flex items-center">
                   <button
                     @click="toggleDarkMode"
                     class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150"
