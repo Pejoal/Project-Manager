43f31c9e992e6999cb53926f34608206238ce1ad
pejoal <pejoal.official@gmail.com>
2025-05-01 21:13:50 +0200

    Refactor button padding and adjust layout spacing for improved responsiveness

--- resources/js/Components/Locales.vue
+++ resources/js/Components/Locales.vue
@@ -14,7 +14,7 @@ const toggleDropdown = () => {
       <button
         @click="toggleDropdown"
         type="button"
-        class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm p-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         id="options-menu"
         aria-haspopup="true"
         aria-expanded="true"
@@ -51,9 +51,7 @@ const toggleDropdown = () => {
               :hreflang="locale.code"
               :class="[
                 'block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900',
-                locale.code === $page.props.active_locale_code
-                  ? 'font-bold'
-                  : '',
+                locale.code === $page.props.active_locale_code ? 'font-bold' : '',
               ]"
               role="menuitem"
             >
--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -51,18 +51,18 @@ const toggleDarkMode = () => {
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
       <nav class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
         <!-- Primary Navigation Menu -->
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto px-1 sm:px-2 lg:px-3">
           <div class="flex justify-between h-16">
             <div class="flex">
               <!-- Logo -->
               <div class="shrink-0 flex items-center">
                 <Link :href="route('dashboard')">
-                  <ApplicationMark class="block h-9 w-auto" />
+                  <ApplicationMark class="block h-8 w-auto" />
                 </Link>
               </div>
 
               <!-- Navigation Links -->
-              <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+              <div class="hidden space-x-2 sm:-my-px sm:ms-4 md:flex">
                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')"> Dashboard </NavLink>
                 <NavLink :href="route('clients.index')" :active="route().current('clients.*')"> Clients </NavLink>
                 <NavLink :href="route('projects.index')" :active="route().current('projects.*')"> Projects </NavLink>
@@ -83,7 +83,8 @@ const toggleDarkMode = () => {
               </div>
             </div>
 
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
+            <!-- Teams & Settings Dropdowns -->
+            <div class="hidden md:flex sm:items-center sm:ms-6">
               <div class="ms-3 relative">
                 <!-- Teams Dropdown -->
                 <Dropdown v-if="$page.props.jetstream.hasTeamFeatures" align="right" width="60">
@@ -252,7 +253,7 @@ const toggleDarkMode = () => {
             </div>
 
             <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
+            <div class="-me-2 flex items-center md:hidden">
               <button
                 class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out"
                 @click="showingNavigationDropdown = !showingNavigationDropdown"
