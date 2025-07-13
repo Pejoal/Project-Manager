70fb551031111d7cdc90d718a48a8c60fb8bdf6b
pejoal <pejoal.official@gmail.com>
2025-07-13 21:29:49 +0200

    Fix duration display in time entries by using formatDuration function

--- resources/js/Pages/TimeTracking/Index.vue
+++ resources/js/Pages/TimeTracking/Index.vue
@@ -206,7 +206,7 @@
                     </td>
                     <td class="px-6 py-4 whitespace-nowrap">
                       <div class="text-sm text-gray-900 dark:text-gray-100">
-                        {{ entry.formatted_duration || '0:00' }}
+                        {{ formatDuration(entry.duration_minutes) || '0:00' }}
                       </div>
                     </td>
                     <td class="px-6 py-4 whitespace-nowrap">
