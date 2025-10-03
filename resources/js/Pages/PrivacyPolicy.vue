aa4f437155ef657f4e00bf1b24f1158f661096f8
pejoal <pejoal.official@gmail.com>
2025-10-03 20:11:35 +0200

    feat: Add layout definition to Privacy Policy and Terms of Service components

--- resources/js/Pages/PrivacyPolicy.vue
+++ resources/js/Pages/PrivacyPolicy.vue
@@ -1,9 +1,15 @@
 <script setup>
 import AuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
+import Layout from '@/Layouts/Layout.vue';
 import { Head } from '@inertiajs/vue3';
 
 defineProps({
   policy: String,
+});
+
+
+defineOptions({
+  layout: Layout,
 });
 </script>
 
--- resources/js/Pages/TermsOfService.vue
+++ resources/js/Pages/TermsOfService.vue
@@ -1,9 +1,14 @@
 <script setup>
 import AuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
+import Layout from '@/Layouts/Layout.vue';
 import { Head } from '@inertiajs/vue3';
 
 defineProps({
   terms: String,
+});
+
+defineOptions({
+  layout: Layout,
 });
 </script>
 
