9c28d0939fe60cf9c128c5547cd46e97c174196d
pejoal <pejoal.official@gmail.com>
2025-10-03 18:27:42 +0200

    feat: Add active prop to DropdownLink for dynamic styling

--- resources/js/Components/DropdownLink.vue
+++ resources/js/Components/DropdownLink.vue
@@ -4,32 +4,26 @@ import { Link } from '@inertiajs/vue3';
 defineProps({
   href: String,
   as: String,
+  active: Boolean, // New prop to toggle active styling
 });
 </script>
 
 <template>
-  <div>
-    <button
-      v-if="as == 'button'"
-      type="submit"
-      class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out"
-    >
+  <div
+    :class="[
+      'block px-4 py-2 text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out',
+      active ? 'font-bold underline text-gray-900 dark:text-white' : '',
+    ]"
+  >
+    <button v-if="as == 'button'" type="submit">
       <slot />
     </button>
 
-    <a
-      v-else-if="as == 'a'"
-      :href="href"
-      class="block px-4 py-2 text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out"
-    >
+    <a v-else-if="as == 'a'">
       <slot />
     </a>
 
-    <Link
-      v-else
-      :href="href"
-      class="block px-4 py-2 text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out"
-    >
+    <Link v-else :href="href">
       <slot />
     </Link>
   </div>
