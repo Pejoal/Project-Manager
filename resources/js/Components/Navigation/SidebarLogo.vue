b6b79c0495697fa3708b29adcf811674bbfe6894
pejoal <pejoal.official@gmail.com>
2025-10-05 19:29:40 +0200

    refactor: Update SidebarLogo component to change displayed text and comment out logo image

--- resources/js/Components/Navigation/SidebarLogo.vue
+++ resources/js/Components/Navigation/SidebarLogo.vue
@@ -14,8 +14,8 @@ const emit = defineEmits(['close-mobile-menu']);
 <template>
   <div class="flex items-center h-16 flex-shrink-0 px-4 bg-gray-900 dark:bg-gray-800">
     <Link :href="route('dashboard')" class="flex items-center" @click="$emit('close-mobile-menu')">
-      <img v-if="!isCollapsed" class="h-8 w-auto" src="/favicon.ico" alt="Company Logo" />
-      <span v-if="!isCollapsed" class="ml-3 text-white font-semibold text-lg">Project Manager</span>
+      <!-- <img v-if="!isCollapsed" class="h-8 w-auto" src="/favicon.ico" alt="Company Logo" /> -->
+      <span v-if="!isCollapsed" class="ml-3 text-white font-semibold text-lg">ERP System</span>
     </Link>
   </div>
 </template>
