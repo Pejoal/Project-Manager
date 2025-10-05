ffd2f79d83f77be86ea42170a87584afe3eb4d39
pejoal <pejoal.official@gmail.com>
2025-10-05 16:36:10 +0200

    fix: Adjust grid layout in filters section to use three columns

--- resources/js/Components/DataTable.vue
+++ resources/js/Components/DataTable.vue
@@ -930,7 +930,7 @@ onMounted(() => {
     <!-- Filters Section -->
     <Transition name="slide-left">
       <div v-if="filterConfig.length > 0 && filtersVisible" class="bg-white dark:bg-gray-800 shadow rounded-lg p-6">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           <div v-for="filter in filterConfig" :key="filter.key">
             <InputLabel v-if="filter.type !== 'checkbox'" :for="filter.key" :value="filter.label" />
 
